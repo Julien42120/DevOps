29a33e19772b40b17685ddb6980f69c54c311b5a
Julien42120 <julienmartin42120@gmail.com>
2022-08-04 14:01:58 +0200

    DEPLOY TO OVH TEST

--- pwa/pages/index.tsx
+++ pwa/pages/index.tsx
@@ -31,7 +31,7 @@ const Welcome = () => (
         </div>
         <div className="main__content">
           <h1>
-            Welcome to <strong>DevOps DEPLOY OVH</strong>!
+            Welcome to <strong>DevOps DEPLOY</strong>!
           </h1>
           <div className="main__before-starting">
             <p>
