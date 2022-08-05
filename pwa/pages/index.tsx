b8423bf833d32d9891ba15d5f3fa89a12c4e7f25
Julien42120 <julienmartin42120@gmail.com>
2022-08-05 11:26:06 +0200

    DEPLOY TO VERCEL Final

--- pwa/pages/index.tsx
+++ pwa/pages/index.tsx
@@ -31,7 +31,7 @@ const Welcome = () => (
         </div>
         <div className="main__content">
           <h1>
-            Welcome to <strong>Projects DevOps DEPLOY Fucking Vercel</strong>!
+            Welcome to <strong>Projects DevOps DEPLOY Fucking Vercel 2 </strong>!
           </h1>
           <div className="main__before-starting">
             <p>
